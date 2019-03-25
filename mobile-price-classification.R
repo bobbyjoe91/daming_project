37bf4138d83ad7058b89ed083b5d52ca46359564
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-03-25 20:30:31 +0700

    numerical data are binned

--- mobile-price-classification.R
+++ mobile-price-classification.R
@@ -1,5 +1,5 @@
 # PEMBACAAN DATA
-setwd("~/Documents/Daming/Project")
+setwd("S:/kuliah/daming_project")
 data_set <- read.csv("train.csv", header = TRUE, sep = ",")
 mobile_price <- data.frame(data_set)
 
@@ -21,7 +21,30 @@ mobile_price$touch_screen <- as.factor(mobile_price$touch_screen)
 mobile_price$wifi <- as.factor(mobile_price$wifi)
 mobile_price$price_range <- as.factor(mobile_price$price_range)
 
-summary(mobile_price)
+categorized <- mobile_price
+summary(categorized)
+View(categorized)
+
+#binning
+library(infotheo)
+binned <- categorized
+binned$battery_power <- cut(mobile_price$battery_power, 15, include.lowest = TRUE)
+binned$clock_speed <- cut(mobile_price$clock_speed, 5, include.lowest = TRUE)
+binned$fc <- cut(mobile_price$fc, 5, include.lowest = TRUE)
+binned$int_memory <- cut(mobile_price$int_memory, 10, include.lowest = TRUE)
+binned$m_dep <- cut(mobile_price$m_dep, 5, include.lowest = TRUE)
+binned$mobile_wt <- cut(mobile_price$mobile_wt, 10, include.lowest = TRUE)
+binned$n_cores <- cut(mobile_price$n_cores, 4, include.lowest = TRUE)
+binned$pc <- cut(mobile_price$pc, 5, include.lowest = TRUE) #
+binned$px_height <- cut(mobile_price$px_height, 5, include.lowest = TRUE) # kok ada 0 nya?
+binned$px_width <- cut(mobile_price$px_width, 10, include.lowest = TRUE)
+binned$ram <- cut(mobile_price$ram, 10, include.lowest = TRUE)
+binned$sc_h <- cut(mobile_price$sc_h, 5, include.lowest = TRUE)
+binned$sc_w <- cut(mobile_price$sc_w, 5, include.lowest = TRUE) # kok ada 0 nya?
+binned$talk_time <- cut(mobile_price$talk_time, 6, include.lowest = TRUE)
+
+View(binned)
+
 #test_data
 #train_data
 
